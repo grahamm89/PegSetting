7303cb8272ad83b27c2563a40b613c27aa95a821
grahamm89 <116651536+grahamm89@users.noreply.github.com>
2025-09-27 06:33:16 +0100

    x

--- js/pwa.js
+++
@@ -1,7 +0,0 @@
-
-// Simple PWA registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch(console.warn);
-  });
-}
